7599f5a2d3cded381f3b826f5caa11d6ab2efd92
zachary-D <zacharyd1@live.com>
2017-09-19 22:01:34 -0400

    Completed Exercise 1

--- Lab 04/frame4.cpp
+++ Lab 04/frame4.cpp
@@ -1,22 +1,27 @@
 // Program Frame4 reads input values that represent the
 // dimensions of a print from a file and calculates and
 // prints the amount of wood needed for a frame.
-#include "stdafx.h"
+
 #include <iostream>
 #include <fstream>
+
 using namespace std;
-int main ()
+
+int main()
 {
-ifstream inFile;
-inFile.open("Frame.in");
-int side; // Vertical dimension in inches
-int top; // Horizontal dimension in inches
-int inchesOfWood; // Inches of wood needed
-inFile >> side >> top;
-cout << "Dimensions are " << top << " and "
-<< side << "." << endl;
-inchesOfWood = top + top + side + side;
-cout << "You need " << inchesOfWood <<" inches of wood."
-<< endl;
-return 0;
+	ifstream inFile;
+	inFile.open("Frame.in");
+	int side; // Vertical dimension in inches
+	int top; // Horizontal dimension in inches
+	int inchesOfWood; // Inches of wood needed
+	inFile >> side >> top;
+	cout << "Dimensions are " << top << " and "
+		<< side << "." << endl;
+	inchesOfWood = top + top + side + side;
+	cout << "You need " << inchesOfWood << " inches of wood."
+		<< endl;
+
+	cin.get();
+
+	return 0;
 }
