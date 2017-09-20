f2d0b991110c57cabceeba1567d3a9789403f5fd
zachary-D <zacharyd1@live.com>
2017-09-19 21:56:59 -0400

    Completed Exercise 7

--- Lab 04/charread.cpp
+++ Lab 04/charread.cpp
@@ -1,21 +1,26 @@
 // Program CharRead prompts for and reads four characters 
 // from the keyboard and then prints them.         
 
-#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 int main ()
 {
-  char  char1;
-  char  char2;
-  char  char3;
-  char  char4;
+	char  char1;
+	char  char2;
+	char  char3;
+	char  char4;
 
-  cout << "Input four characters.  Press Return."  << endl;
-  cin >> char1  >> char2  >> char3  >> char4;
-  cout << char1  << char2  << char3  << char4;
-  return 0;
+	cout << "Input four characters.  Press Return." << endl;
+	cin.get(char1);
+	cin.get(char2);
+	cin.get(char3);
+	cin.get(char4);
+	cout << char1 << char2 << char3 << char4;
+
+	cin.ignore(100, '\n');
+
+	return 0;
 }
 
 
